beac2e36008b86245d73185f216c18c84b3013da
shulikin <alex@shulikin.com>
2025-01-23 22:11:45 +0400

    230125

--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -1,9 +1,9 @@
 from typing import Optional
+from typing import List
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import func, select
 
-from app.core.config import Constant
 from app.crud.base import CRUDBaseAdvanced
 from app.models.charity_project import CharityProject
 
@@ -20,27 +20,23 @@ class CRUDCharityProject(CRUDBaseAdvanced):
         return await self.get_by_kwargs(session, name=project_name)
 
     async def get_projects_by_completion_rate(
-            self, session: AsyncSession
-    ) -> list[list]:
-        """Получение списка проектов."""
+        self, session: AsyncSession
+    ) -> List[List[str]]:
+        """Получение списка проектов, отсортированных по времени выполнения."""
         project_objs = await session.execute(
-            select(CharityProject).where(
+            select(
+                CharityProject.name,
+                (func.julianday(CharityProject.close_date) - func.julianday(
+                    CharityProject.create_date)).label('completion_days'),
+                CharityProject.description,
+            ).where(
                 CharityProject.close_date.isnot(None)
-            ).order_by(
-                func.coalesce(
-                    CharityProject.close_date - CharityProject.create_date,
-                    Constant.RETURNED_NULL
-                )
-            )
+            ).order_by('completion_days')
         )
-        project_objs = project_objs.scalars().all()
+        projects = project_objs.all()
         return [
-            [
-                project.name,
-                str(project.close_date - project.create_date),
-                project.description
-            ]
-            for project in project_objs
+            [project.name, project.completion_days, project.description]
+            for project in projects
         ]
 
 
